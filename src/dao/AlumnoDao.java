a71eea8a9ef731b3d2a6774606950a64fd471a70
jennifer2511 <113562384+jennifer2511@users.noreply.github.com>
2022-11-10 20:39:19 -0500

    Evaluacion Continua 2

---
+++ src/dao/AlumnoDao.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import java.util.List;
+import entity.Alumno;
+import java.util.ArrayList;
+/**
+ *
+ * @author Alumno
+ */
+public class AlumnoDao {
+    private List<Alumno> lista;
+    
+    public AlumnoDao(){
+        lista = new ArrayList<>(); 
+    }
+    
+    public void add(Alumno alum){
+        lista.add(alum);
+    }
+
+    public List<Alumno> getLista() {
+        return lista;
+    }
+    
+    public void actualizar(int index, Alumno alum) {
+        lista.get(index).setNombre(alum.getNombre());
+        lista.get(index).setApellidos(alum.getApellidos());
+        lista.get(index).setCarrera(alum.getCarrera());
+        lista.get(index).setCiclo(alum.getCiclo());
+        lista.get(index).setTurno(alum.getTurno());
+        lista.get(index).setFecha(alum.getFecha());
+        lista.get(index).setModalidad(alum.getModalidad());
+    }
+    
+    public void eliminar(int index){
+        lista.remove(index);
+    }
+    
+    public int buscar(String nombre){
+        int index=-1;
+        for(int i=0;i<lista.size();i++){
+            if(lista.get(i).getNombre().equals(nombre));
+            index=i;
+        }
+        return index;
+    }
+}
---
+++ src/entity/Alumno.java
@@ -0,0 +1,90 @@
+    /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Alumno {
+    private String nombre;
+    private String apellidos;
+    private String carrera;
+    private String ciclo;
+    private String turno;
+    private String fecha;
+    private String modalidad;
+
+    public Alumno() {
+    }
+
+    public Alumno(String nombre, String apellidos, String carrera, String ciclo, String turno, String fecha, String modalidad) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.carrera = carrera;
+        this.ciclo = ciclo;
+        this.turno = turno;
+        this.fecha = fecha;
+        this.modalidad = modalidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+
+    public String getCiclo() {
+        return ciclo;
+    }
+
+    public void setCiclo(String ciclo) {
+        this.ciclo = ciclo;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getModalidad() {
+        return modalidad;
+    }
+
+    public void setModalidad(String modalidad) {
+        this.modalidad = modalidad;
+    }
+    
+    
+}
